dcc20133fa65ad367fb74eee14649e24812b70e2
wiut00016599 <wiut00016599@gmail.com>
2024-02-01 01:22:26 +0500

    updated

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main> {children} </main>
+        <main className="mx-5"> {children} </main>
       </body>
     </html>
   );
